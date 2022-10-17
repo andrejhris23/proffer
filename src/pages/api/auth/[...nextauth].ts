b6150a3728741dec90a714ad85feed1ec73ab3b3
Andrej Hristov <andrejhris23@gmail.com>
2022-10-17 18:09:12 +0200

    Setup next-auth with wrapper for unstable session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,4 +2,26 @@ import NextAuth, { type NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-// import { prisma } from 
+import { prisma } from "@/src/server/db/client";
+
+export const authOptions: NextAuthOptions = {
+  // Include user id and role on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+        session.user.role = user.role;
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!
+    })
+  ]
+};
+
+export default NextAuth(authOptions);
