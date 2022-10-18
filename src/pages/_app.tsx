85fd8a457c48085301bcb16d006962f9c29ac548
Andrej Hristov <andrejhris23@gmail.com>
2022-10-18 15:23:24 +0200

    Import type session instead of object

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
-import type { AppType } from 'next/app'
-import { Session } from 'next-auth'
+import '../styles/globals.css';
+import type { AppType } from 'next/app';
+import type { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react'; 
 
 const MyApp: AppType<{ session: Session | null }> = ({
