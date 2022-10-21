06af7164d0545f9fa5e7ed57de5cbff9bef8358c
Andrej Hristov <andrejhris23@gmail.com>
2022-10-21 23:48:00 +0200

    Add rendering logic to home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import { useSession } from "next-auth/react";
+
+// TODO: render a welcome screen if user role is undefined, if not render the screen depending on the role of the user
+const Home: NextPage = () => {
+  const {data: session} = useSession();
+  if(!session?.user?.role){
+    // render welcome screen, trpc call to set role
+    return (
+      <>
+      Please choose if this is you're first time joining!
+      </>
+    )
+  }
+
+  return(
+    <div>
+    {  session.user.role === 'AGENT' && <>You&aposre an agent. Congrats!</>}
+    {  session.user.role === 'TALENT' && <>You&aposre a talent. Good luck with finding a job!</>}
+    </div> 
+  );
+}
+
+export default Home;
