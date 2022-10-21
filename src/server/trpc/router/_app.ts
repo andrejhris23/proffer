13b1019304ad6166322f438389924b261ec322b7
Andrej Hristov <andrejhris23@gmail.com>
2022-10-21 23:57:42 +0200

    Create user router, define queries and mutations

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,7 +1,9 @@
 import { router } from '../trpc';
+import { userRouter } from './user';
 
 export const appRouter = router({
   // all of your routers will go here
+  user: userRouter,
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/server/trpc/router/user.ts
@@ -0,0 +1,78 @@
+import { router, protectedProcedure } from '../trpc';
+import { TRPCError } from '@trpc/server';
+import { Prisma, Role } from '@prisma/client';
+
+const defaultTalentValidator = Prisma.validator<Prisma.UserSelect>()({
+  id: true,
+  email: true,
+  image: true,
+  applications: true
+});
+
+const defaultAgentValidator = Prisma.validator<Prisma.UserSelect>()({
+  id: true,
+  email: true,
+  image: true,
+  offers: true
+});
+
+export const userRouter = router({
+  getAgent: protectedProcedure
+  .query(async ({ ctx }) => {
+    return await ctx.prisma.user.findUnique({
+      where: {
+        id: ctx.session.user.id
+      },
+      select: defaultAgentValidator
+    });
+  }),
+  getTalent: protectedProcedure
+  .query(async ({ ctx }) => {
+    return await ctx.prisma.user.findUnique({
+      where: {
+        id: ctx.session.user.id
+      },
+      select: defaultTalentValidator
+    });
+  }),
+  setRoleAsAgent: protectedProcedure
+  .mutation(async ({ ctx }) => {
+    if(ctx.session.user.role) {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'you can not change your role once it has been set'
+      });
+    }
+
+    await ctx.prisma.user.update({
+      where: {
+        id: ctx.session.user.id
+      },
+      data: {
+        role: Role.AGENT
+      }
+    });
+
+    return 'Successfully set role to Agent!';
+  }),
+  setRoleAsTalent: protectedProcedure
+  .mutation(async ({ ctx }) => {
+    if(ctx.session.user.role) {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'you can not change your role once it has been set'
+      });
+    }
+
+    await ctx.prisma.user.update({
+      where: {
+        id: ctx.session.user.id
+      },
+      data: {
+        role: Role.TALENT
+      }
+    });
+
+    return 'Successfully set role to Talent!';
+  }),
+});
