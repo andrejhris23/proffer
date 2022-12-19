b4a55863549b7cd805f1fce071ba0a737557b168
Andrej Hristov <andrejhris23@gmail.com>
2022-12-19 16:53:15 +0100

    Change bg of html element

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-// TODO: when migrating to the app directory, this can be done at the root layout.tsx file (a custom document component is no longer needed)
+// TODO: when migrating to the app directory, this can be done at the root layout.tsx file (a custom document page is no longer needed)
 export default function Document() {
   return (
-    <Html className='h-full bg-gray-50'>
+    <Html className='h-full bg-white'>
       <Head />
       <body className='h-full overflow-hidden'>
         <Main />
