47b1355a703d498d75105394f5063243f8b35b19
Andrej Hristov <andrejhris23@gmail.com>
2022-12-26 01:00:58 +0100

    Add layout to home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,31 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
+import Layout from "../components/layouts/Layout";
+import ChooseRoleScreen from "../components/screens/ChooseRoleScreen";
 
 // TODO: render a welcome screen if user role is undefined, if not render the screen depending on the role of the user
 const Home: NextPage = () => {
-  const {data: session} = useSession();
-  if(!session?.user?.role){
-    // render welcome screen, trpc call to set role
-    return (
-      <>
-      Please choose if this is you're first time joining!
-      </>
-    )
-  }
+  // const {data: session} = useSession();
+  // if(!session?.user?.role){
+  //   // render welcome screen, trpc call to set role
+  //   return (
+  //     <>
+  //     Please choose if this is you're first time joining!
+  //     </>
+  //   )
+  // }
 
-  return(
-    <div>
-    {  session.user.role === 'AGENT' && <>You&aposre an agent. Congrats!</>}
-    {  session.user.role === 'TALENT' && <>You&aposre a talent. Good luck with finding a job!</>}
-    </div> 
-  );
+  // return(
+  //   <div>
+  //   {  session.user.role === 'AGENT' && <>You&aposre an agent. Congrats!</>}
+  //   {  session.user.role === 'TALENT' && <>You&aposre a talent. Good luck with finding a job!</>}
+  //   </div> 
+  // );
+  return (
+    <Layout>
+      <ChooseRoleScreen/>
+    </Layout>
+    );
 }
 
 export default Home;
