8d02f3ae5b1987f579795e999a68b00935e1e014
Andrej Hristov <andrejhris23@gmail.com>
2022-12-26 01:01:22 +0100

    Add offer router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,11 @@
 import { router } from '../trpc';
 import { userRouter } from './user';
+import { offerRouter } from './offer';
 
 export const appRouter = router({
   // all of your routers will go here
   user: userRouter,
+  offer: offerRouter,
 });
 
 export type AppRouter = typeof appRouter;
