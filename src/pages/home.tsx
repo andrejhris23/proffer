3041a8670c7cdfcd08dae468308ef485252c26ac
Andrej Hristov <andrejhris23@gmail.com>
2023-01-03 18:06:37 +0100

    Initial logic for home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,30 +2,24 @@ import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Layout from "../components/layouts/Layout";
 import ChooseRoleScreen from "../components/screens/ChooseRoleScreen";
+import AgentHomeScreen from "../components/screens/AgentHomeScreen";
+import TalentHomeScreen from "../components/screens/TalentHomeScreen";
 
-// TODO: render a welcome screen if user role is undefined, if not render the screen depending on the role of the user
+// TODO: change this style of rendering screens, switch to middleware for role checking maybe
 const Home: NextPage = () => {
-  // const {data: session} = useSession();
-  // if(!session?.user?.role){
-  //   // render welcome screen, trpc call to set role
-  //   return (
-  //     <>
-  //     Please choose if this is you're first time joining!
-  //     </>
-  //   )
-  // }
+  const {data: session} = useSession();
 
-  // return(
-  //   <div>
-  //   {  session.user.role === 'AGENT' && <>You&aposre an agent. Congrats!</>}
-  //   {  session.user.role === 'TALENT' && <>You&aposre a talent. Good luck with finding a job!</>}
-  //   </div> 
-  // );
-  return (
+  if(!session?.user?.role){
+  // render welcome screen, trpc call to set role
+    return <ChooseRoleScreen />;
+  }
+
+  return(
     <Layout>
-      <ChooseRoleScreen/>
+      { session.user.role === 'AGENT' && <AgentHomeScreen /> }
+      { session.user.role === 'TALENT' && <TalentHomeScreen /> }
     </Layout>
-    );
+  );
 }
 
 export default Home;
