5b68b22e68d0e6d52ed9360f202651fe9d271f2f
Andrej Hristov <andrejhris23@gmail.com>
2023-01-05 14:40:05 +0100

    Add helpers for infering route inputs and outputs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,6 +2,7 @@ import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from '@/src/server/api/root';
 import superjson from 'superjson';
+import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''; // browser should use relative url
@@ -27,3 +28,14 @@ export const api = createTRPCNext<AppRouter>({
   },
   ssr: false,
 });
+
+/**
+ * Inference helper for inputs
+ * @example type HelloInput = RouterInputs['example']['hello']
+ */
+export type RouterInputs = inferRouterInputs<AppRouter>;
+/**
+ * Inference helper for outputs
+ * @example type HelloOutput = RouterOutputs['example']['hello']
+ */
+export type RouterOutputs = inferRouterOutputs<AppRouter>;
