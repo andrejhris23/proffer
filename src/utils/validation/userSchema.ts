391d3a0183c5133f5c6101b443bcaf762f3a257c
Andrej Hristov <andrejhris23@gmail.com>
2023-01-10 20:19:46 +0100

    Add schema for user properties

--- src/utils/validation/userSchema.ts
+++ src/utils/validation/userSchema.ts
@@ -1,10 +1,27 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-  email: z.string().email().optional(),
-  image: z.string().optional(),
-  firstName: z.string().optional(),
-  lastName: z.string().optional(),
-  website: z.string().optional(),
-  bio: z.string().optional()
+  email: z
+    .string()
+    .email('This is not a valid email address!').optional(),
+  image: z
+    .string()
+    .url('This is not a valid url address!')
+    .optional(),
+  firstName: z
+    .string()
+    .min(1, 'First Name must be atleast 1 character long!')
+    .optional(),
+  lastName: z
+    .string()
+    .min(1, 'Last Name must be atleast 1 character long!')
+    .optional(),
+  website: z
+    .string()
+    .url('Website must be a valid url address!')
+    .optional(),
+  bio: z
+    .string()
+    .min(1, 'Bio must be atleast 1 character long!')
+    .optional(),
 });
